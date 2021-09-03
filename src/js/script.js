babf20bee63d877f524f9de6b9816fffe6d808d9
Andreza Ferreira <andrezafs.rr@gmail.com>
2021-09-03 12:12:54 -0400

    adição de algumas animações

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+const header = ScrollReveal({
+    origin: "top",
+    distance: "50px",
+    duration: 4000,
+    reset: true,
+  });
+  header.reveal("#header", { delay: 100 });
+  header.reveal("#logos", { delay: 50 });
+  header.reveal("#about", { delay: 50 });
+  header.reveal(".img", { delay: 30 });
+  header.reveal("#portifolio", { delay: 50});
+  header.reveal("#coments", { delay: 50 });
+  header.reveal(".coments-container-box", { delay: 80});
+  header.reveal("#cta", { delay: 50 });
+  header.reveal("#footer", { delay: 50 });
+
+const bg = ScrollReveal({
+    origin: "top",
+    distance: "50px",
+    duration: 10000,
+    reset: true,
+  });
+  bg.reveal("#hero", { delay: 10 });
+
+
+  const rigth = ScrollReveal({
+    origin: "rigth",
+    distance: "50px",
+    duration: 6000,
+    reset: true,
+  });
+  rigth.reveal(".about-grid", { delay: 50 });
+  
