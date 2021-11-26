4478dc7803b9b7fe83d323d3fba95be9dde66cf5
Andreza Ferreira <andrezafs.rr@gmail.com>
2021-11-26 19:46:42 -0400

    style: ajuste de scrollreavel

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,17 @@
 const header = ScrollReveal({
   origin: "top",
   distance: "50px",
-  duration: 4000,
+  duration: 2000,
   reset: true,
 });
-header.reveal("#header", { delay: 100 });
-header.reveal("#logos", { delay: 50 });
-header.reveal("#about", { delay: 50 });
-header.reveal(".img", { delay: 30 });
+header.reveal("#header", { delay: 0 });
+header.reveal("#logos", { delay: 200 });
+header.reveal("#about", { delay: 0 });
+header.reveal(".img", { delay: 20 });
 header.reveal("#portifolio", { delay: 50 });
 header.reveal("#coments", { delay: 50 });
+header.reveal(".hero", { delay: 100 });
+
 
 header.reveal("#cta", { delay: 50 });
 header.reveal("#footer", { delay: 50 });
@@ -17,19 +19,19 @@ header.reveal("#footer", { delay: 50 });
 const bg = ScrollReveal({
   origin: "top",
   distance: "50px",
-  duration: 10000,
+  duration: 2000,
   reset: true,
 });
-bg.reveal("#hero", { delay: 10 });
+bg.reveal("#hero", { delay: 0 });
 
 
 const rigth = ScrollReveal({
   origin: "rigth",
   distance: "50px",
-  duration: 6000,
+  duration: 3000,
   reset: true,
 });
-rigth.reveal(".about-grid", { delay: 50 });
+rigth.reveal(".about-grid", { delay: 0 });
 
 
 
